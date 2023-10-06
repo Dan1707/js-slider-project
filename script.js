89ef872142fb4035d8d6078e9f949ab36db7da62
Dan1707 <danuhae@gmail.com>
2023-10-06 20:33:43 +0300

    commit

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
+const sliderContainer = document.querySelector(".slider__item-content");
 const sliderLine = document.querySelector(".slider__item-line");
+const sliderWidthWrapper = document.querySelector(".slider__item-wrapper");
 const prevBtn = document.querySelector(`#prev`);
 const nextBtn = document.querySelector(`#next`);
 
-let sliderWidth = 0;
+const sliderArray = [
+  {
+    color: `df4343`,
+    text: `1`,
+  },
+  {
+    color: `00ff00`,
+    text: `2`,
+  },
+  {
+    color: `4400ff`,
+    text: `3`,
+  },
+];
+
+function sliderBuilder() {
+  let html = "";
+  sliderArray.forEach((el) => {
+    html += `<div class="slider__item" style="background-color:#${el.color};">${el.text}</div>`;
+  });
+  sliderContainer.innerHTML = html;
+}
+
+sliderBuilder();
+
+const sliderWidth = window
+  .getComputedStyle(sliderWidthWrapper, null)
+  .getPropertyValue("width");
+
+const sliderWidthNumber = parseFloat(sliderWidth);
+
+let sliderGap = 0;
 
 nextBtn.addEventListener("click", () => {
-  sliderWidth = sliderWidth - 700;
-  if (sliderWidth <= -2100) {
-    sliderWidth = 0;
+  sliderGap = sliderGap - sliderWidthNumber;
+  if (sliderGap <= -sliderWidthNumber * sliderArray.length) {
+    sliderGap = 0;
   }
-  sliderLine.setAttribute("style", `left: ${sliderWidth}px`);
+  sliderLine.style.left = `${sliderGap}px`;
 });
 
 prevBtn.addEventListener("click", () => {
-  sliderWidth = sliderWidth + 700;
-  if (sliderWidth > 0) {
-    sliderWidth = -1400;
+  sliderGap = sliderGap + sliderWidthNumber;
+  if (sliderGap > 0) {
+    sliderGap = -sliderWidthNumber * sliderArray.length + sliderWidthNumber;
   }
-  sliderLine.setAttribute("style", `left: ${sliderWidth}px`);
+  sliderLine.style.left = `${sliderGap}px`;
 });
